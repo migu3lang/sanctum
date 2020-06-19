ef66985c2c08d220fa1c93d12e67518a228694c9
migu3lang <miguelapm92@gmail.com>
2020-06-19 15:06:42 -0500

    nadita

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/pacho', function (Request $request) {
+    dd('nelson');
+});
+
 
 Route::post('/register', 'RegisterController@register');
 Route::post('/login', 'LoginController@login');
